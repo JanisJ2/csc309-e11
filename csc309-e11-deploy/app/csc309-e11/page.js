32eb007cfc305225ff123064e1208d22f47c4401
JanisJ2 <janisjoplin22404@gmail.com>
2024-11-28 01:57:59 -0500

    Add utorid

--- csc309-e11-deploy/app/csc309-e11/page.js
+++ csc309-e11-deploy/app/csc309-e11/page.js
@@ -1,8 +1,8 @@
 export default function AutoTester() {
   return (
     <div id="auto-tester">
-      Hi. I am YOUR_UTORID, and I have learned how to develop a full-stack web
-      app and deploy it!
+      Hi. I am joplinja, and I have learned how to develop a full-stack web app
+      and deploy it!
     </div>
   );
 }
